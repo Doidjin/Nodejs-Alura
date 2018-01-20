b806d68eaff39778dce45c7fafd721d3faef89ce
Doidjin <LeonardoCabbau@MacBook-Air-de-LEONARDO.local>
2018-01-20 18:05:16 -0200

    Testando o cadastro de produtos / Utilizando os testes em um banco exclusivo / Passando os erros para o Express e criando o banco / Limpando as tabelas antes da execucao no caso de testes

--- test/produtos.js
+++ test/produtos.js
@@ -3,12 +3,32 @@ var request = require('supertest')(express);
 
 describe('ProdutosController',function(){
 
-    it('listagem json',function(done){
+    beforeEach(function(done){
+        var conn = express.infra.connectionFactory();
+        conn.query("delete from produtos", function(ex, result){
+            if(!ex){
+                done();
+            }
+        });
+    });
 
+    it('listagem json',function(done){
     request.get('/produtos')
             .set('Accept','application/json')
             .expect('Content-Type',/json/)
             .expect(200, done);
     
+    });
+
+    it('cadastro de novos produtos com dados invalidos', function(done){
+        request.post('/produtos')
+            .send({titulo:"", descricao:"teste"})
+            .expect(400, done);
+    });
+
+    it('cadastro de novos produtos com dados validos', function(done){
+        request.post('/produtos')
+            .send({titulo:"titulo", descricao:"descricao", preco:20.50})
+            .expect(302, done);
     });
 });
